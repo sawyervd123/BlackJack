9a3e5159b7af9bbeb86ba4df04ff55568726fb3e
Sawyervd <88403568+Sawyervd@users.noreply.github.com>
2024-07-31 21:39:01 -0400

    Updates to the API

--- src/game.js
+++ src/game.js
@@ -15,16 +15,13 @@ let playerCardSprites = [], dealerCardSprites = [], jokeText;
 
 function preload() {
     this.load.image('cardBack', '/assets/images/card_back_red.png');
-    this.load.atlasXML('cards', '/assets/images/playingCards.png', '/assets/playingCards.xml'); // Replace with actual paths
+    this.load.atlasXML('cards', '/assets/images/playingCards.png', '/assets/playingCards.xml');
 }
 
 function create() {
-    this.cameras.main.setBackgroundColor('#228B22');
-    
+    setBackgroundColorBasedOnState(this);
     this.add.text(config.width / 2, 50, 'Blackjack Game', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-
     fetchRandomJoke(this);
-
     startButton = this.add.text(config.width / 2, 160, 'Start Game', { fontSize: '32px', fill: '#f00' })
         .setOrigin(0.5)
         .setInteractive()
@@ -114,7 +111,6 @@ function playerStand() {
 }
 
 function displayHand(scene, hand, x, y, owner, hideSecondCard = false) {
-
     if (owner === 'Player') {
         playerCardSprites.forEach(sprite => sprite.destroy());
         playerCardSprites = [];
@@ -146,31 +142,23 @@ function getCardImageKey(card) {
 
 function resetGame() {
     gameOver = false;
-
-
     deck = createDeck();
     shuffleDeck(deck);
-
     playerHand = [drawCard(), drawCard()];
     dealerHand = [drawCard(), drawCard()];
-
     displayHand(this, playerHand, 150, 450, 'Player');
     displayHand(this, dealerHand, 150, 200, 'Dealer', true);
     playerText.setText(`Player: ${getHandValue(playerHand)}`);
     dealerText.setText(`Dealer: ${getHandValue([dealerHand[0]])} + ?`);
     messageText.setText('');
-
     hitButton.setInteractive();
     standButton.setInteractive();
 }
 
 function startGame(scene) {
     if (jokeText) jokeText.destroy();
-
     initGame(scene);
-
     startButton.setVisible(false);
-    
     hitButton.setVisible(true);
     standButton.setVisible(true);
     resetButton.setVisible(true);
@@ -181,14 +169,11 @@ function initGame(scene) {
     shuffleDeck(deck);
     playerHand = [drawCard(), drawCard()];
     dealerHand = [drawCard(), drawCard()];
-
     displayHand(scene, playerHand, 150, 450, 'Player');
     displayHand(scene, dealerHand, 150, 200, 'Dealer', true);
-
     playerText = scene.add.text(150, 550, `Player: ${getHandValue(playerHand)}`, { fontSize: '32px', fill: '#fff' });
     dealerText = scene.add.text(150, 100, `Dealer: ${getHandValue([dealerHand[0]])} + ?`, { fontSize: '32px', fill: '#fff' });
     messageText = scene.add.text(500, 650, '', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-
     hitButton = scene.add.text(800, 450, 'Hit', { fontSize: '32px', fill: '#0f0' })
         .setInteractive()
         .on('pointerdown', playerHit, scene);
@@ -198,8 +183,77 @@ function initGame(scene) {
     resetButton = scene.add.text(800, 550, 'Reset', { fontSize: '32px', fill: '#f00' })
         .setInteractive()
         .on('pointerdown', resetGame, scene);
-
     hitButton.setVisible(false);
     standButton.setVisible(false);
     resetButton.setVisible(false);
+}
+
+function setBackgroundColorBasedOnState(scene) {
+    fetch('https://ipinfo.io/json?token=${token}') 
+        .then(response => response.json())
+        .then(data => {
+            const state = data.region;
+            const color = getColorByState(state);
+            scene.cameras.main.setBackgroundColor(color);
+        })
+        .catch(error => {
+            console.error('Error fetching location data:', error);
+        });
+}
+
+function getColorByState(state) {
+    const stateColors = {
+        'Alabama': '#FF5733',
+        'Alaska': '#33C4FF',
+        'Arizona': '#FF33FF',
+        'Arkansas': '#33FF57',
+        'California': '#FFDDC1',
+        'Colorado': '#FFD733',
+        'Connecticut': '#C1C1FF',
+        'Delaware': '#33FFDD',
+        'Florida': '#FF33AA',
+        'Georgia': '#FFA833',
+        'Hawaii': '#33FFA8',
+        'Idaho': '#FFA833',
+        'Illinois': '#FF5733',
+        'Indiana': '#33C4FF',
+        'Iowa': '#FF33FF',
+        'Kansas': '#33FF57',
+        'Kentucky': '#FFDDC1',
+        'Louisiana': '#FFD733',
+        'Maine': '#C1C1FF',
+        'Maryland': '#33FFDD',
+        'Massachusetts': '#FF33AA',
+        'Michigan': '#FFA833',
+        'Minnesota': '#33FFA8',
+        'Mississippi': '#FFA833',
+        'Missouri': '#FF5733',
+        'Montana': '#33C4FF',
+        'Nebraska': '#FF33FF',
+        'Nevada': '#33FF57',
+        'New Hampshire': '#FFDDC1',
+        'New Jersey': '#FFD733',
+        'New Mexico': '#C1C1FF',
+        'New York': '#33FFDD',
+        'North Carolina': '#FF33AA',
+        'North Dakota': '#FFA833',
+        'Ohio': '#33FFA8',
+        'Oklahoma': '#FFA833',
+        'Oregon': '#FF5733',
+        'Pennsylvania': '#33C4FF',
+        'Rhode Island': '#FF33FF',
+        'South Carolina': '#33FF57',
+        'South Dakota': '#FFDDC1',
+        'Tennessee': '#FFD733',
+        'Texas': '#FFC1C1',
+        'Utah': '#C1D1FF',
+        'Vermont': '#33FFDD',
+        'Virginia': '#FF33AA',
+        'Washington': '#FFA833',
+        'West Virginia': '#33FFA8',
+        'Wisconsin': '#FFA833',
+        'Wyoming': '#FF5733',
+        'default': '#228B22'
+    };
+    return stateColors[state] || stateColors['default'];
 }
